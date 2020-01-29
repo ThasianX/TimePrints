c72044c8c60fee6abcf863909efdf05cebd7d421
Kevin Li <kevinli29033@gmail.com>
2020-01-28 19:37:39 -0500

    Initial Commit

---
+++ Locations App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Locations App
+//
+//  Created by Kevin Li on 1/28/20.
+//  Copyright © 2020 Kevin Li. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Locations AppTests/Locations_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Locations_AppTests.swift
+//  Locations AppTests
+//
+//  Created by Kevin Li on 1/28/20.
+//  Copyright © 2020 Kevin Li. All rights reserved.
+//
+
+import XCTest
+@testable import Locations_App
+
+class Locations_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
