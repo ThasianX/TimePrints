cfcc7129a363b855b88c0d9f322bde4ecd02bbfc
Kevin Li <kevinli29033@gmail.com>
2020-02-15 22:53:01 -0500

    Visit details view finished animations

--- Locations App/Views/Screens/Day Details/Components/VisitDetailsView.swift
+++ Locations App/Views/Screens/Day Details/Components/VisitDetailsView.swift
@@ -3,6 +3,7 @@ import SwiftUI
 struct VisitDetailsView: View {
     @State private var mapFull = false
     @State private var isFavorite = false
+    @State private var addNotesShowing = false
     @Binding var selectedIndex: Int
 
     let index: Int
@@ -12,22 +13,14 @@ struct VisitDetailsView: View {
         ZStack(alignment: .top) {
             VStack {
                 header
-                    .padding(.bottom, isSelected ? 20 : 0)
-                visitDurationText
-                fullMonthWithDayOfWeekTextIfSelected
-                    .padding(.bottom, isSelected ? 10 : 0)
-                locationTagView
-                Group {
-                    if isSelected {
-                        staticMapView
-                        locationAddressText
-                    }
-                }
+                visitDetails
+                interactableMapViewIfSelected
+                notesIfSelected
                 Spacer()
             }
             .padding(.top, isSelected ? 80 : 12)
-            .padding(.leading, isSelected ? 30 : 40)
-            .padding(.trailing, isSelected ? 30 : 40)
+            .padding(.leading, isSelected ? 0 : 40)
+            .padding(.trailing, isSelected ? 0 : 40)
             .frame(height: VisitCellConstants.height(if: isSelected))
             .frame(maxWidth: VisitCellConstants.maxWidth(if: isSelected))
             .background(Color(UIColor.salmon))
@@ -54,10 +47,19 @@ private extension VisitDetailsView {
             favoriteButton
                 .fade(!isSelected)
         }
+        .padding(.bottom, isSelected ? 20 : 0)
+        .padding(.leading, isSelected ? 30 : 0)
+        .padding(.trailing, isSelected ? 30 : 0)
     }
 
     private var backButton: some View {
-        BImage(perform: unselectRow, image: .init(systemName: "arrow.left"))
+        ZStack {
+            Color(.white)
+                .fade(!mapFull)
+            BImage(perform: mapFull ? minimizeMap : unselectRow, image: .init(systemName: "arrow.left"))
+        }
+        .frame(width: 30, height: 30)
+        .clipShape(RoundedRectangle(cornerRadius: 15, style: .continuous))
     }
 
     private var locationNameText: some View {
@@ -92,7 +94,21 @@ private extension VisitDetailsView {
     }
 }
 
-extension VisitDetailsView {
+private extension VisitDetailsView {
+    private var visitDetails: some View {
+        Group {
+            if !mapFull {
+                visitDurationText
+                fullMonthWithDayOfWeekTextIfSelected
+                    .padding(.bottom, isSelected ? 10 : 0)
+                locationTagView
+                    .padding(.bottom, isSelected ? 20 : 0)
+            }
+        }
+        .scaleEffect(mapFull ? 0 : 1)
+        .fade(mapFull)
+    }
+
     private var visitDurationText: some View {
         Text(visit.visitDuration)
             .font(isSelected ? .system(size: 18) : .system(size: 10))
@@ -108,6 +124,10 @@ extension VisitDetailsView {
         }
     }
 
+    private var fullMonthWithDayOfWeekText: some View {
+        Text(visit.arrivalDate.fullMonthWithDayOfWeek.uppercased()).font(.caption)
+    }
+
     private var locationTagView: some View {
         TagView(tag: visit.location.tag, displayName: isSelected)
             .padding(.init(top: 6, leading: 0, bottom: 4, trailing: 0))
@@ -115,13 +135,26 @@ extension VisitDetailsView {
 }
 
 private extension VisitDetailsView {
-    private var fullMonthWithDayOfWeekText: some View {
-        Text(visit.arrivalDate.fullMonthWithDayOfWeek.uppercased()).font(.caption)
+    private var interactableMapViewIfSelected: some View {
+        Group {
+            if isSelected {
+                staticMapView
+                    .padding(.bottom, 10)
+                Group {
+                    locationAddressText
+                    mapOptionButtons
+                        .fade(!mapFull)
+                        .scaleEffect(mapFull ? 1 : 0)
+                }
+                .padding(.leading, 80)
+                .padding(.trailing, 80)
+            }
+        }
     }
 
     private var staticMapView: some View {
         StaticMapView(coordinate: visit.location.coordinate, name: visit.location.name, color: .blue)
-            .frame(width: mapFull ? screen.width : screen.width / 2.5, height: mapFull ? screen.height * 3 / 4 : screen.width / 2.5)
+            .frame(width: mapFull ? screen.width : screen.width / 2.5, height: mapFull ? screen.height * 8 / 15 : screen.width / 2.5)
             .cornerRadius(mapFull ? 0 : screen.width / 5)
             .onTapGesture(perform: toggleMapState)
             .animation(.spring())
@@ -130,11 +163,99 @@ private extension VisitDetailsView {
     private var locationAddressText: some View {
         Text(visit.location.address.uppercased())
             .font(.headline)
+            .lineLimit(nil)
             .multilineTextAlignment(.center)
+    }
+
+    private var mapOptionButtons: some View {
+        HStack(spacing: 20) {
+            focusLocationOnAnnotatedMapButton
+            openAppleMapsButton
+        }
+    }
+
+    private var openAppleMapsButton: some View {
+        Button(action: openAppleMaps) {
+            Image(systemName: "arrow.up.right.diamond")
+                .resizable()
+                .frame(width: 35, height: 35)
+        }
+        .buttonStyle(PlainButtonStyle())
+    }
+
+    private var focusLocationOnAnnotatedMapButton: some View {
+        Button(action: focusLocationOnAnnotatedMap) {
+            Image(systemName: "magnifyingglass")
+                .resizable()
+                .frame(width: 35, height: 35)
+        }
+        .buttonStyle(PlainButtonStyle())
     }
 }
 
-extension VisitDetailsView {
+private extension VisitDetailsView {
+    private var notesIfSelected: some View {
+        Group {
+            if isSelected {
+                notesButton
+                    .fade(mapFull)
+                    .scaleEffect(mapFull ? 0 : 1)
+            }
+        }
+    }
+
+    private var notesButton: some View {
+        Button(action: displayAddNotesView) {
+            VStack(spacing: 2) {
+                dividerView
+                    .padding(.bottom, 20)
+
+                notesHeaderText
+
+                visitNotesTextWithDefaultIfEmpty
+
+                dividerView
+                    .padding(.top, 20)
+            }
+        }
+        .buttonStyle(PlainButtonStyle())
+    }
+
+    private var dividerView: some View {
+        RoundedRectangle(cornerRadius: 20)
+            .fill(Color.black)
+            .frame(width: 60, height: 3)
+    }
+
+    private var notesHeaderText: some View {
+        Text("NOTES")
+            .font(.system(size: 22))
+            .fontWeight(.bold)
+            .tracking(2)
+    }
+
+    private var visitNotesTextWithDefaultIfEmpty: some View {
+        Group {
+            if visit.notes.isEmpty {
+                emptyNotesText
+            } else {
+                visitNotesText
+            }
+        }
+    }
+
+    private var emptyNotesText: some View {
+        Text("TAP TO ADD")
+            .font(.caption)
+    }
+
+    private var visitNotesText: some View {
+        Text(visit.notes)
+            .font(.caption)
+    }
+}
+
+private extension VisitDetailsView {
     private func setFavoriteState() {
         isFavorite = visit.isFavorite
     }
@@ -147,6 +268,12 @@ extension VisitDetailsView {
 
     private var isSelected: Bool {
         selectedIndex == index
+    }
+
+    private func minimizeMap() {
+        withAnimation {
+            self.mapFull = false
+        }
     }
     
     private func unselectRow() {
@@ -165,6 +292,18 @@ extension VisitDetailsView {
         withAnimation {
             mapFull.toggle()
         }
+    }
+
+    private func openAppleMaps() {
+
+    }
+
+    private func focusLocationOnAnnotatedMap() {
+
+    }
+
+    private func displayAddNotesView() {
+        addNotesShowing = true
     }
 }
 
