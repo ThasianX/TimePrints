ec56a7bf956c102741dbd8f6d73b8398c12f4f46
Kevin Li <kevinli29033@gmail.com>
2020-02-28 10:50:22 -0500

    Added a login view

---
+++ Locations App/Views/Screens/LoginView.swift
@@ -0,0 +1,15 @@
+// Kevin Li - 10:46 AM - 2/28/20
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
