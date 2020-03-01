42dcba6ad5910749a402fc310cfc8eb98afd7e92
Kevin Li <kevinli29033@gmail.com>
2020-02-29 20:31:04 -0500

    Proper ordering for ICloudLoginService properties and methods

--- Locations App/ICloud/Login/ICloudLoginService.swift
+++ Locations App/ICloud/Login/ICloudLoginService.swift
@@ -14,6 +14,10 @@ final class ICloudLoginService: LoginService {
         completion(isICloudAvailable)
     }
 
+    func logOut() {
+        ICloudKVS.Account.set(false, for: .isUserLoggedIn)
+    }
+    
     var isICloudAvailable: Bool {
         // Opaque token that represents the user's iCloud identity; nil if not logged into iCloud
         FileManager.default.ubiquityIdentityToken != nil
@@ -21,9 +25,5 @@ final class ICloudLoginService: LoginService {
 
     private func logInUser() {
         ICloudKVS.Account.set(true, for: .isUserLoggedIn)
-    }
-
-    func logOut() {
-        ICloudKVS.Account.set(false, for: .isUserLoggedIn)
     }
 }
