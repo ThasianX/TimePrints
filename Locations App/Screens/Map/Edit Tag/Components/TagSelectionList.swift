510e6d7a445a96d9dc1a0c52a4ef3053a434d3a8
kevin.li <kevin.li@duethealth.com>
2020-04-18 19:49:02 -0700

    Tag Context menu is now a reusable component

--- Locations App/Screens/Map/Edit Tag/Components/TagSelectionList.swift
+++ Locations App/Screens/Map/Edit Tag/Components/TagSelectionList.swift
@@ -31,46 +31,12 @@ struct TagSelectionList: View {
                 self.onSelect(tag)
             }
             .contextMenu {
-                self.contextMenu(for: tag)
+                TagContextMenu(tagState: tagState, tag: tag)
             }
     }
 
     private func coloredTextRow(tag: Tag) -> TagRow {
-        let isSelected = selectedLocationTag == tag
-        return TagRow(tag: tag, isSelected: isSelected)
-    }
-
-    private func contextMenu(for tag: Tag) -> some View {
-        VStack {
-            editButton(for: tag)
-            deleteButton(for: tag)
-        }
-    }
-
-    private func editButton(for tag: Tag) -> some View {
-        Button(action: { self.tagState.operation.setTagToEdit(tag) }) {
-            editTextPrecededByPencil
-        }
-    }
-
-    private var editTextPrecededByPencil: some View {
-        HStack {
-            Text("Edit")
-            Image(systemName: "pencil")
-        }
-    }
-
-    private func deleteButton(for tag: Tag) -> some View {
-        Button(action: { self.tagState.removeTag(tag) }) {
-            deleteTextPrecededByTrash
-        }
-    }
-
-    private var deleteTextPrecededByTrash: some View {
-        HStack {
-            Text("Delete")
-            Image(systemName: "trash.fill")
-        }
+        TagRow(tag: tag, isSelected: selectedLocationTag == tag)
     }
 }
 
