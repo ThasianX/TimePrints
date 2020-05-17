de028eaf8bc9ffad3188242de8b85b574ba591c5
Kevin Li <kevinli29033@gmail.com>
2020-05-16 17:47:25 -0700

    refactored tag details view

---
+++ Locations App/Screens/Visits Home View/Tags/TagDetailsHeaderView.swift
@@ -0,0 +1,107 @@
+// Kevin Li - 5:46 PM - 5/16/20
+
+import SwiftUI
+
+struct TagDetailsHeaderView: View {
+    let tag: Tag
+
+    var body: some View {
+        tagDetailsHeaderView
+    }
+
+    private var tagDetailsHeaderView: some View {
+        HStack {
+            tagDetailsStack
+            Spacer()
+            numberOfFavoritedVisitsView
+        }
+    }
+}
+
+private extension TagDetailsHeaderView {
+    private var tagDetailsStack: some View {
+        VStack(alignment: .leading) {
+            tagNameText
+            numberOfLocationsText
+            numberOfVisitsText
+        }
+        .animation(nil)
+    }
+
+    private var tagNameText: some View {
+        Text(tag.name)
+            .font(.headline)
+            .foregroundColor(.white)
+    }
+
+    private var numberOfLocationsText: some View {
+        Text("\(numberOfLocations) locations")
+            .font(.caption)
+            .foregroundColor(.white)
+    }
+
+    private var numberOfLocations: Int {
+        tag.locations.count
+    }
+
+    private var numberOfVisitsText: some View {
+        Text("\(numberOfVisits) visits")
+            .font(.caption)
+            .foregroundColor(.white)
+    }
+
+    private var numberOfVisits: Int {
+        tag.locations.reduce(0, { count, location in
+            count + location.visits.count
+        })
+    }
+}
+
+private extension TagDetailsHeaderView {
+    private var numberOfFavoritedVisitsView: some View {
+        ZStack {
+            starImage
+                .padding(8)
+                .foregroundColor(.yellow)
+                .overlay(
+                    Circle()
+                        .stroke(Color.white, lineWidth: 1)
+                )
+
+            numberOfFavoritedVisitsText
+                .padding(6)
+                .background(Color.black)
+                .clipShape(Circle())
+                .foregroundColor(.white)
+                .offset(x: 12, y: -16)
+        }
+    }
+
+    private var starImage: some View {
+        Group {
+            if numberOfFavoritedVisits == 0 {
+                Image(systemName: "star")
+                    .resizable()
+                    .frame(width: 20, height: 20)
+            } else {
+                Image(systemName: "star.fill")
+                    .resizable()
+                    .frame(width: 20, height: 20)
+            }
+        }
+    }
+
+    private var numberOfFavoritedVisitsText: some View {
+        Text("\(numberOfFavoritedVisits)")
+            .font(.system(size: 9))
+            .foregroundColor(.white)
+    }
+
+    private var numberOfFavoritedVisits: Int {
+        tag.locations.reduce(0, { favoritedVisits, location in
+            favoritedVisits + location.visits.reduce(0, { favorited, visit in
+                favorited + (visit.isFavorite ? 1 : 0)
+            })
+        })
+    }
+}
--- Locations App/Screens/Visits Home View/Tags/TagDetailsView.swift
+++ Locations App/Screens/Visits Home View/Tags/TagDetailsView.swift
@@ -13,7 +13,8 @@ struct TagDetailsView: View {
 
     var body: some View {
         tagDetailsView
-            .padding(12)
+            .padding(.top, isSelected ? 80 : 12)
+            .padding([.bottom, .horizontal], 12)
             .frame(height: TagCellConstants.height(if: isSelected))
             .frame(maxWidth: TagCellConstants.maxWidth(if: isSelected))
             .extendToScreenEdges()
@@ -27,103 +28,13 @@ struct TagDetailsView: View {
 private extension TagDetailsView {
     private var tagDetailsView: some View {
         VStack {
-            HStack {
-                HStack(spacing: 12) {
-                    tagDetailsStack
-                }
-                Spacer()
-
-                numberOfFavoritedVisitsView
-            }
-        }
-    }
-}
-
-private extension TagDetailsView {
-    private var tagDetailsStack: some View {
-        VStack(alignment: .leading) {
-            tagNameText
-            numberOfLocationsText
-            numberOfVisitsText
-        }
-        .animation(nil)
-    }
-
-    private var tagNameText: some View {
-        Text(tag.name)
-            .font(.headline)
-            .foregroundColor(.white)
-    }
-
-    private var numberOfLocationsText: some View {
-        Text("\(numberOfLocations) locations")
-            .font(.caption)
-            .foregroundColor(.white)
-    }
-
-    private var numberOfLocations: Int {
-        tag.locations.count
-    }
-
-    private var numberOfVisitsText: some View {
-        Text("\(numberOfVisits) visits")
-            .font(.caption)
-            .foregroundColor(.white)
-    }
-
-    private var numberOfVisits: Int {
-        tag.locations.reduce(0, { count, location in
-            count + location.visits.count
-        })
-    }
-}
-
-private extension TagDetailsView {
-    private var numberOfFavoritedVisitsView: some View {
-        ZStack {
-            starImage
-                .padding(8)
-                .foregroundColor(.yellow)
-                .overlay(
-                    Circle()
-                        .stroke(Color.white, lineWidth: 1)
-                )
-
-            numberOfFavoritedVisitsText
-                .padding(6)
-                .background(Color.black)
-                .clipShape(Circle())
-                .foregroundColor(.white)
-                .offset(x: 12, y: -16)
+            tagDetailsHeaderView
+            Spacer()
         }
     }
 
-    private var starImage: some View {
-        Group {
-            if numberOfFavoritedVisits == 0 {
-                Image(systemName: "star")
-                    .resizable()
-                    .frame(width: 20, height: 20)
-            } else {
-                Image(systemName: "star.fill")
-                    .resizable()
-                    .frame(width: 20, height: 20)
-            }
-        }
-    }
-
-    private var numberOfFavoritedVisitsText: some View {
-        Text("\(numberOfFavoritedVisits)")
-            .font(.system(size: 9))
-            .foregroundColor(.white)
-    }
-
-    private var numberOfFavoritedVisits: Int {
-        tag.locations.reduce(0, { favoritedVisits, location in
-            favoritedVisits + location.visits.reduce(0, { favorited, visit in
-                favorited + (visit.isFavorite ? 1 : 0)
-            })
-        })
+    private var tagDetailsHeaderView: TagDetailsHeaderView {
+        TagDetailsHeaderView(tag: tag)
     }
 }
 
